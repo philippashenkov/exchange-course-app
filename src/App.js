503279202ec67bf379c7f28f7782bc367d62517b
philippashenkov <reikoslvl@gmail.com>
2020-08-03 22:11:22 +0300

    calc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
+import Rate from './Rate/Rate';
+
+class App extends React.Component {
+  // constructor(props) {
+  //   super(props);
+  // }
+  render() {
+    return(
+      <div className="site">
+        <Header />
+      <div className="container">
+          <main>
+            <Rate />
+          </main>
+        </div>
+              
+        <div className="container" id="cookie_info">
+          <div className="site-content">
+            <div className="well">На нашем сайте мы используем cookie для сбора
+            информации технического характера.<br/>В
+                частности, для персонифицированной работы сайта мы 
+                обрабытываем IP-адрес региона вашего
+            местоположения.&nbsp;<button className="btn btn-primary btn-sm">OK</
+            button></div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
